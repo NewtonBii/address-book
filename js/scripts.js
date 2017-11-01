35ecdc4f88662d8ef14175dd0a267ad0c581cf0a
Newton Bii <biinewton382@gmail.com>
2017-11-01 11:30:24 +0300

    Add first behavior to js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 //businessLogic
 function Places(country, location, means, landmark, month, time) {
-  this.country = country;
-  this.location = location;
-  this.means = means;
-  this.landmark = landmark;
-  this.month = month;
-  this.time = time;
+  var obj={};
+  obj.country = country;
+  obj.location = location;
+  obj.means = means;
+  obj.landmark = landmark;
+  obj.month = month;
+  obj.time = time;
+  obj.describe=function () {
+    alert("It is Located in "+ this.country+", " +this.location);
+  };
+  return obj;
 }
 var maasaiMara = new Places("Kenya","Narok County",["Road","Air"],"Mara River","November","Afternoon");
 var malindi = new Places("Kenya","Mombasa County",["Road","Air"],"Indian Ocean","December","Noon");
@@ -13,5 +18,6 @@ var zanzibar = new Places("Tanzania","Zanzibar",["Road","Air","Water"],"Indian O
 var sychelles = new Places("Sychelles","Southern Africa",["Road","Air","Water"],"Indian Ocean","August","Noon");
 var morocco = new Places("Morocco", "Northen Africa",["Road","Air", "Water"],"Rabat","May","Evening");
 
+
 //userEndLogic
-$()
+maasaiMara.describe();
