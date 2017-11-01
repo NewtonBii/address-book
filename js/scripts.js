cb6ff9e96f675d46a2305becc302535d903d30d6
Newton Bii <biinewton382@gmail.com>
2017-11-01 11:47:47 +0300

    add onclick event attribute.

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,15 @@ function Places(country, location, means, landmark, month, time) {
   obj.month = month;
   obj.time = time;
   obj.describe=function () {
-    alert("It is Located in "+ this.country+", " +this.location);
+    alert("It is Located in "+ this.country+", " +this.location + " and you can travel by" + this.means + ". The first feature you are likely to see is" + this.landmark + " which is awesome. The best time to travel is " + this.month + " in the " + this.time+ ".");
   };
   return obj;
 }
-var maasaiMara = new Places("Kenya","Narok County",["Road","Air"],"Mara River","November","Afternoon");
-var malindi = new Places("Kenya","Mombasa County",["Road","Air"],"Indian Ocean","December","Noon");
-var zanzibar = new Places("Tanzania","Zanzibar",["Road","Air","Water"],"Indian Ocean","August","Noon");
-var sychelles = new Places("Sychelles","Southern Africa",["Road","Air","Water"],"Indian Ocean","August","Noon");
-var morocco = new Places("Morocco", "Northen Africa",["Road","Air", "Water"],"Rabat","May","Evening");
+var maasaiMara = new Places("Kenya","Narok County","Road or Air","Mara River","November","Afternoon");
+var malindi = new Places("Kenya","Mombasa County","Road or Air","Indian Ocean","December","Noon");
+var zanzibar = new Places("Tanzania","Zanzibar","Road or Air","Water","Indian Ocean","August","Noon");
+var sychelles = new Places("Sychelles","Southern Africa","Road, Air or Water","Indian Ocean","August","Noon");
+var morocco = new Places("Morocco", "Northen Africa","Road, Air or Water","Rabat","May","Evening");
 
 
 //userEndLogic
-maasaiMara.describe();
