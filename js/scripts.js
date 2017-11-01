46ed8d0096aa892da8f30ac41212205138a07a91
Newton Bii <biinewton382@gmail.com>
2017-11-01 07:19:38 +0300

    Add user end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
+//business logic
+
 function Contact(first,second){
   this.firstName=first;
   this.secondName=second;
 }
+
+//userInterface
+$("form#new-contact").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+    });
