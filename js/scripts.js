4a922e0a722a4f7f2fd7acb7478971877a9385a4
Newton Bii <biinewton382@gmail.com>
2017-11-01 14:26:28 +0300

    Add event.

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ function Places(country, location, means, landmark, month, time) {
   obj.month = month;
   obj.time = time;
   obj.describe=function () {
-    alert("It is Located in "+ this.country+", " +this.location + " and you can travel by " + this.means + ". The first feature you are likely to see is " + this.landmark + " which is awesome. The best time to travel is " + this.month + " in the " + this.time+ ".");
+    $("#display").append("It is Located in "+ this.country+", " +this.location + "you can travel by " + this.means + ". The first feature you are likely to see is " + this.landmark + " which is awesome. The best time to travel is " + this.month + " in the " + this.time+ ".");
   };
   return obj;
 }
@@ -17,6 +17,3 @@ var malindi = new Places("Kenya","Mombasa County","Road or Air","Indian Ocean","
 var zanzibar = new Places("Tanzania","Zanzibar","Road or Air","Water","Indian Ocean","August","Noon");
 var sychelles = new Places("Sychelles","Southern Africa","Road, Air or Water","Indian Ocean","August","Noon");
 var morocco = new Places("Morocco", "Northen Africa","Road, Air or Water","Rabat","May","Evening");
-
-
-//userEndLogic
